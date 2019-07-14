cc88aba005b97a609fd74557a442f8d36eba235f
Volodymyr Havryliuk <volodymyr.havryliuk@student.put.poznan.pl>
2019-07-14 17:08:09 +0300

    Added API.

---
+++ Model/TodoContext.cs
@@ -0,0 +1,18 @@
+﻿using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApi.Model
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
